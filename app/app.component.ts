8e4d11df0cd6930dbcb4b85544c9aa9958f5415b
BigDaddy <wagner820@gmx.com>
2016-12-23 03:43:48 +0100

    list showing based on service with mockup heroes list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES : Hero[] = [
-	{id: 11, name: 'Mr.Nice'},
-	{id: 12, name: 'Narco'},
-	{id: 13, name: 'Bombasto'},
-	{id: 14, name: 'Celeritas'},
-	{id: 15, name: 'Magneta'},
-	{id: 16, name: 'RubberMan'},
-	{id: 17, name: 'Dynama'},
-	{id: 18, name: 'Dr IQ'},
-	{id: 19, name: 'Magma'},
-	{id: 20, name: 'Tornado'}
-];
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'my-app',
+	providers: [HeroService],
 	template: `
  		<h1>{{title}}</h1>
 		<h2>My Heroes</h2>
@@ -78,11 +67,20 @@ const HEROES : Hero[] = [
 		}
 	`]
 })
-export class AppComponent  {
+
+export class AppComponent implements OnInit {
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes: Hero[];
 	selectedHero: Hero;
+
+	constructor(private heroService: HeroService) {}
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+	getHeroes(): void {
+		this.heroes = this.heroService.getHeroes();
+	}
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
