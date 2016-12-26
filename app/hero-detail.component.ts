f1a98eb7d964877d7bc694cbe982d558422a27c5
BigDaddy <wagner820@gmx.com>
2016-12-26 15:42:03 +0100

    styles applied

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -4,16 +4,15 @@ import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 
 import { HeroService } from './hero.service';
-import { Hero } from './hero';
-
 
 @Component({
 	moduleId: module.id,
 	selector: 'my-hero-detail',
-	templateUrl: 'hero-detail.component.html'
+	templateUrl: 'hero-detail.component.html',
+	styleUrls: ['hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-	@Input() hero: Hero;
+	hero: Hero;
 
 	constructor(
 		private heroService: HeroService,
