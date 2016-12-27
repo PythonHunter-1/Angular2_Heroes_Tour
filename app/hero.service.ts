ac5b88438cff283b53543d6638085d072b3dd601
BigDaddy <wagner820@gmx.com>
2016-12-27 04:45:20 +0100

    initial CRUD applied to service and heroes, details

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -31,7 +31,37 @@ export class HeroService {
 		})
 	}
 	getHero(id: number): Promise<Hero> {
-		return this.getHeroes()
-				.then(heroes => heroes.find(hero => hero.id === id));
+		// return this.getHeroes()
+		// 		.then(heroes => heroes.find(hero => hero.id === id));
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.get(url)
+			.toPromise()
+			.then(response => response.json().data as Hero)
+			.catch(this.handleError);
+	}
+
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+				.put(url, JSON.stringify(hero), { headers: this.headers })
+				.toPromise()
+				.then(() => hero)
+				.catch(this.handleError);
+	}
+	create(name: String): Promise<Hero> {
+		return this.http
+				.post(this.heroesUrl, JSON.stringify({name: name}), { headers: this.headers })
+				.toPromise()
+				.then(res => res.json().data)
+				.catch(this.handleError);
+	}
+	delete(id: number): Promise<void> {
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.delete(url, { headers: this.headers })
+				.toPromise()
+				.then(() => null)
+				.catch(this.handleError);
 	}
 }
